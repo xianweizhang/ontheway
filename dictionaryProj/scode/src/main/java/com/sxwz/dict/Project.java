c7edeaadeb40a2639fd3a716d7ffbc105ea614be
xianweizhang <ontheway1248@gmail.com>
2014-06-03 16:01:13 +0800

    initialize the dict project
    2. add a java project

---
+++ dictionaryProj/scode/src/main/java/com/sxwz/dict/Project.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.sxwz.dict;
+
+/**
+ * @author ontheway1248@gmail.com
+ *
+ */
+public final class Project {
+
+    /**
+     * 
+     */
+    public Project() {
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+       System.out.println("Dictionary project!");
+
+    }
+
+}
